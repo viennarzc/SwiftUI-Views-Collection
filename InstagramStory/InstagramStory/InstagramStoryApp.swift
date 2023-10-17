41466df1ab9d9b0daefecc039b6344cbd13da3cf
Viennarz Curtiz <viennarzc@gmail.com>
2023-10-17 19:59:50 +0800

    Add Project - Instagram Story

---
+++ InstagramStory/InstagramStory/InstagramStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoryApp.swift
+//  InstagramStory
+//
+//  Created by Viennarz Curtiz on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
